8f8eb26f668dc2ceceeec37e4cd0ce1fcd16a84a
heyharshjaiswal <harshjaiswal2307@gmail.com>
2022-03-31 01:46:46 +0530

    update .gitignore

--- UserAuth/env.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST_USER = 'testsender2307@gmail.com'
-EMAIL_HOST_PASSWORD = 'test@2022'
-SECRET_KEY = 'django-insecure-pjniw8ae$81^yz%$q&w3@rehujs9)lkb=ecwxk0u+_94i6r%8x'
-DEBUG = True
-ALLOWED_HOSTS = []
